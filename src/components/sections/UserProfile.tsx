3696c2d46a2d214e7f0dd061b55a93a1d9dc3819
shameCanales <canalesshame@gmail.com>
2025-12-16 11:53:41 +0800

    fixed userProfile content bug

--- src/components/sections/UserProfile.tsx
+++ src/components/sections/UserProfile.tsx
@@ -1,4 +1,5 @@
 import { useGetProfileData } from "@/hooks/useGetProfileData";
+import { ReactNode } from "react";
 
 export default function UserProfile() {
   const {
@@ -8,7 +9,7 @@ export default function UserProfile() {
     error: userError,
   } = useGetProfileData();
 
-  let profileContent: any = "";
+  let profileContent: ReactNode = null;
 
   if (user) {
     profileContent = (
@@ -26,11 +27,24 @@ export default function UserProfile() {
   }
 
   if (loadingUser) {
-    profileContent = <h1>Loading...</h1>;
+    profileContent = (
+      <>
+        <div>
+          <div className="bg-slate-200 animate-pulse aspect-square rounded-full w-9 ml-2"></div>
+        </div>
+
+        <div className="ml-3">
+          <div className="h-4 w-32 bg-slate-200 animate-pulse rounded mb-1"></div>
+          <div className="h-3 w-20 bg-slate-200 animate-pulse rounded"></div>
+        </div>
+      </>
+    );
   }
 
   if (loadingUserError) {
-    profileContent = <p>{userError.message}</p>;
+    profileContent = (
+      <p className="text-red-500 ml-2 text-sm">{userError.message}</p>
+    );
   }
   return (
     <div className="flex items-center mt-3 border-y-slate-300 border-y py-4">
